bd0a97c323158cd99598da1fe3b8c5490872bbfe
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 08:19:11 +0400

    Example of for

---
+++ src/chapters/chapter2/ForExample.java
@@ -0,0 +1,10 @@
+package chapters.chapter2;
+
+public class ForExample {
+    public static void main(String[] args) {
+        int x;
+        for ( x = 0; x < 10; x++) {
+            System.out.println(x);
+        }
+    }
+}
