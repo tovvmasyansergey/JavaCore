94d2e8d86e230037e41e2d403d5d5286ac5aad52
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 11:27:49 +0400

    example print

---
+++ src/chapters/chapter2/Example3.java
@@ -0,0 +1,10 @@
+package chapters.chapter2;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("hello " + 10 + 5);
+        System.out.println();
+        System.out.println("hello " + (10 + 5));
+    }
+}
