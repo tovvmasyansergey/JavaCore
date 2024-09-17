af64e2e2aca53415fbff6338908fc5d7a5b5f492
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 07:56:23 +0400

    example second

---
+++ src/chapters/chapter2/Example2.java
@@ -0,0 +1,12 @@
+package chapters.chapter2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("this let is " + num);
+        num = num * 2;
+        System.out.println("this let is num * 2 = " + num);
+
+    }
+}
