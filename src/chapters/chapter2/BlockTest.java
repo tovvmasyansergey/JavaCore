d8c3a4c5ceae1d166e4e42fd5922cc054d93b36a
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 08:21:35 +0400

    example of for

---
+++ src/chapters/chapter2/BlockTest.java
@@ -0,0 +1,13 @@
+package chapters.chapter2;
+
+public class BlockTest {
+    public static void main(String[] args) {
+        int x, y;
+        y = 10;
+        for (x = 0; x < 5; x++) {
+            System.out.println(x);
+            System.out.println(y);
+            y = y - 2;
+        }
+    }
+}
