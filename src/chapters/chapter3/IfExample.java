f6e2a950baa899847e2ce61ef1effe06f6eff0a6
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 07:12:47 +0400

    If example

---
+++ src/chapters/chapter3/IfExample.java
@@ -0,0 +1,13 @@
+package chapters.chapter3;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        if (a > b) {
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
