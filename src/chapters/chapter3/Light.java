93f7659a80fbb1ed195dafa45b68646a6e4d82a5
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 08:27:05 +0400

    example of let

---
+++ src/chapters/chapter3/Light.java
@@ -0,0 +1,15 @@
+package chapters.chapter3;
+
+public class Light {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+        lightspeed = 186000;
+        days = 1000;
+        seconds = days * 24 * 60 * 60;
+        distance = lightspeed * seconds;
+        System.out.println("for " + days + " days,light is go " + distance + " mils");
+    }
+}
