f96a4b3ab2ab6729a66121b59bef873757222ee7
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 08:50:40 +0400

    Example of char

---
+++ src/chapters/chapter3/CharDemo2.java
@@ -0,0 +1,11 @@
+package chapters.chapter3;
+
+public class CharDemo2 {
+    public static void main(String[] args) {
+        char ch1;
+        ch1 = 'X';
+        System.out.println("X = " + ch1);
+        ch1 ++;
+        System.out.println("X != " + ch1 );
+    }
+}
