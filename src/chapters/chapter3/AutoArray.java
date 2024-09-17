3799ce6e9882dce952d39b78460a2d7065ef2cff
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 10:24:52 +0400

    example of array

---
+++ src/chapters/chapter3/AutoArray.java
@@ -0,0 +1,9 @@
+package chapters.chapter3;
+
+public class AutoArray {
+    public static void main(String[] args) {
+        int month_days[] = {31 , 28 , 31 , 30 , 31 , 30, 31 , 31 ,
+                30, 31, 30 , 31};
+        System.out.println("in april " + month_days[1] + "days");
+    }
+}
