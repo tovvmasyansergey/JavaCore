ed41c60073acd38cb7266eca262ac6be554c78e3
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-17 13:33:33 +0400

    homework 1

---
+++ src/homework/homework1/IfForExample.java
@@ -0,0 +1,33 @@
+package homework.homework1;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        //1
+//        if (a > b){
+//            System.out.println(a);
+//        }else {
+//            System.out.println(b);
+//        }
+        //2
+//        if (a > b){
+//            for (int i = b; i <= a; i++) {
+//                System.out.println(i);
+//            }
+//        }else {
+//            for (int i = a; i <= b; i++) {
+//                System.out.println(i);
+//            }
+//        }
+        //3
+//        System.out.println((char)a);
+//        System.out.println((char)b);
+        //4
+        if (a == b){
+            System.out.println("a == b");
+        }else {
+            System.out.println("a != b");
+        }
+    }
+}
