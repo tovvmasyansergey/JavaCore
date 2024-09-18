ed89af60ea7fedf412a3fcbde963f0c5eda9fcf2
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-18 08:54:11 +0400

    example of switch case

---
+++ src/chapters/chapter5/SampleSwitch.java
@@ -0,0 +1,24 @@
+package chapters.chapter5;
+
+public class SampleSwitch {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i = " + i);
+                    break;
+                case 1:
+                    System.out.println("i = " + i);
+                    break;
+                case 2:
+                    System.out.println("i = " + i);
+                    break;
+                case 3:
+                    System.out.println("i = " + i);
+                    break;
+                default:
+                    System.out.println("i > 3");
+            }
+        }
+    }
+}
