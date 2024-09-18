8f9525410590c1c63515d18eee730c3152eb31b3
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-18 07:38:56 +0400

    example of if (?::)

---
+++ src/chapters/chapter4/Ternary.java
@@ -0,0 +1,13 @@
+package chapters.chapter4;
+
+public class Ternary {
+    public static void main(String[] args) {
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i;
+        System.out.println("i = " + k);
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.println("i = " + k);
+    }
+}
