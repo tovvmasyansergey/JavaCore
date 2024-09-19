e93b7bd34cb79bfafc17dfe133416e8a9b49ad24
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-19 09:39:43 +0400

    example of while loop

---
+++ src/chapters/chapter5/Nobody.java
@@ -0,0 +1,12 @@
+package chapters.chapter5;
+
+public class Nobody {
+    public static void main(String[] args) {
+        int i , j ;
+        i = 100;
+        j = 200;
+        while (++i < --j);
+            System.out.println("middle " + i);
+
+    }
+}
