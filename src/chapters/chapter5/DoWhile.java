832bc2ccee392c9c205097bd442da21ee310b0f7
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-19 09:41:46 +0400

    example of while loop

---
+++ src/chapters/chapter5/DoWhile.java
@@ -0,0 +1,12 @@
+package chapters.chapter5;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        do {
+            System.out.println("takt " + n);
+            n--;
+
+        }while (n > 0);
+    }
+}
