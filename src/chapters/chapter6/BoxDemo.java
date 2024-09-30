b81bc128ccd7131827c339b9d71c58d604ad3382
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-09-30 13:37:20 +0400

    example of class

--- src/chapters/chapter6/BoxDemo.java
+++ src/chapters/chapter6/BoxDemo.java
@@ -3,11 +3,14 @@ package chapters.chapter6;
 public class BoxDemo {
     public static void main(String[] args) {
         Box myBox = new Box();
+        Box myBox2 = new Box();
         double volume;
         myBox.width = 2;
         myBox.depth = 2;
         myBox.height = 2;
         volume = myBox.depth * myBox.height * myBox.depth;
+        System.out.println(volume);
+        volume = myBox2.depth * myBox2.height * myBox2.depth;
         System.out.println(volume);
     }
 }
