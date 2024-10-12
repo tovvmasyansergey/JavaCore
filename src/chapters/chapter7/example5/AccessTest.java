21be7eb1db127bdfd598a02f5a1343f9e91c2741
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-12 17:04:05 +0400

    example of encapsulation

---
+++ src/chapters/chapter7/example5/AccessTest.java
@@ -0,0 +1,10 @@
+package chapters.chapter7.example5;
+
+
+public class AccessTest {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.setc(34);
+        System.out.println(test.getc());
+    }
+}
---
+++ src/chapters/chapter7/example5/Test.java
@@ -0,0 +1,13 @@
+package chapters.chapter7.example5;
+
+public class Test {
+    int a;
+    private int b;
+    private int c;
+    void setc(int i){
+        c = i;
+    }
+    int getc(){
+        return c;
+    }
+}
