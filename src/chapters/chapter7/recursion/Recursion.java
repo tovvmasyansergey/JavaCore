2e9116776fc111eb7b8c255594bf7d915169467c
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-12 16:57:45 +0400

    example of recursion

---
+++ src/chapters/chapter7/recursion/Recursion.java
@@ -0,0 +1,8 @@
+package chapters.chapter7.recursion;
+
+public class Recursion {
+    public static void main(String[] args) {
+        Factorial factorial = new Factorial();
+        System.out.println(factorial.fact(5));
+    }
+}
