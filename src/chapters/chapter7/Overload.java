0f2a2e658885430ea72cdf6afc7ef329a219a5ae
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-12 12:14:10 +0400

    example of overload

--- src/chapters/chapter7/Overload.java
+++ src/chapters/chapter7/Overload.java
@@ -5,7 +5,7 @@ public class Overload {
         OverloadDemo overloadDemo = new OverloadDemo();
         overloadDemo.test();
         overloadDemo.test(2);
-        overloadDemo.test(3,4);
+        overloadDemo.test(3, 4);
         double test = overloadDemo.test(23.4);
         System.out.println(test);
     }
