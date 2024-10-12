a93c2d8e18de76a931e5b0615b1c0c076a30c89d
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-12 11:40:30 +0400

    dynamic array

--- src/homework/homework8/DynamicArray.java
+++ src/homework/homework8/DynamicArray.java
@@ -10,9 +10,8 @@ public class DynamicArray {
     //ստուգել եթե մասիվի մեջ տեղ չկա-> կանչել extend()
     //և ավելացնենք
     public void add(int value) {
-        if (size == array.length){
+        if (size == array.length) {
             extend();
-            array[size++] = value;
         }
         array[size++] = value;
     }
@@ -33,7 +32,7 @@ public class DynamicArray {
     // մասիվի index-երրորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
     public int getByIndex(int index) {
         if (index < size && index > 0) {
-            return  array[index];
+            return array[index];
         }
         System.out.println("index is out of length");
         return 0;
@@ -42,9 +41,62 @@ public class DynamicArray {
     //տպել մասիվի ավելացված էլեմենտները
     public void print() {
         for (int i = 0; i < size; i++) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + " ");
         }
-        }
+        System.out.println();
     }
+
+    public void deleteByIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            System.out.println("wrong index");
+            return;
+        }
+        for (int i = index + 1; i < size; i++) {
+            array[i - 1] = array[i];
+        }
+        size--;
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index > size - 1) {
+            System.out.println("wrong index");
+            return;
+        }
+        array[index] = value;
+    }
+
+    public void add(int index, int value) {
+        if (index < 0 || index > size - 1) {
+            System.out.println("wrong index");
+            return;
+        }
+        if (size == array.length) {
+            extend();
+        }
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+        size++;
+    }
+
+    public boolean exists(int value) {
+        for (int i : array) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
 
 
--- src/homework/homework8/DynamicArrayDemo.java
+++ src/homework/homework8/DynamicArrayDemo.java
@@ -2,12 +2,18 @@ package homework.homework8;
 
 public class DynamicArrayDemo {
     public static void main(String[] args) {
-       DynamicArray dy = new DynamicArray();
-       dy.add(3);
-        for (int i = 0; i < 12; i++) {
+        DynamicArray dy = new DynamicArray();
+        for (int i = 0; i < 10; i++) {
             dy.add(i);
         }
         dy.print();
-        System.out.println(dy.getByIndex(4));
+        dy.deleteByIndex(0);
+        dy.set(3, 23);
+        dy.print();
+        dy.add(0, 34);
+        dy.print();
+//        System.out.println(dy.exists(3));
+//        System.out.println(dy.exists(100));
+//        System.out.println(dy.getIndexByValue(34));
     }
 }
