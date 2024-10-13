a101d4c06be72ae4ab014e0918885db1e172afda
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-13 16:48:07 +0400

    example of integer

---
+++ src/chapters/chapter7/IntegerExample.java
@@ -0,0 +1,9 @@
+package chapters.chapter7;
+
+public class IntegerExample {
+    public static void main(String[] args) {
+        String msg = "34";
+        int a = Integer.parseInt(msg);
+        System.out.println(a*2);
+    }
+}
