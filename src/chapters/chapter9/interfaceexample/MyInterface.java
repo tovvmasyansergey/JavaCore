aaaf93f55ba8ed86727ab4d51e234ae69fecf67c
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-28 19:53:32 +0400

    interface

--- src/chapters/chapter9/interfaceexample/MyInterface.java
+++
@@ -1,5 +0,0 @@
-package chapters.chapter9.interfaceexample;
-
-public interface MyInterface {
-    void myMethod();
-}
--- src/chapters/chapter9/interfaceexample/MyInterfaceImpl.java
+++
@@ -1,8 +0,0 @@
-package chapters.chapter9.interfaceexample;
-
-public class MyInterfaceImpl implements MyInterface {
-    @Override
-    public void myMethod() {
-
-    }
-}
