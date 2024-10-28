0605d20dba2a64d8ddbb468eb75931fa81d3da23
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-28 19:54:25 +0400

    wrapper class

---
+++ src/chapters/chapter12/wraper/WrapperClassExample.java
@@ -0,0 +1,10 @@
+package chapters.chapter12.wraper;
+
+public class WrapperClassExample {
+    public static void main(String[] args) {
+        Integer b =900;
+        Integer a = 900;
+        int c = 9;
+        System.out.println(a == b);
+    }
+}
