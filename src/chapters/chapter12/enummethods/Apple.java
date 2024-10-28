6d4c4f7b5263072a9fd6216d7dd737683b7f974f
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-28 19:50:26 +0400

    annotation example of methods

---
+++ src/chapters/chapter12/enummethods/Apple.java
@@ -0,0 +1,9 @@
+package chapters.chapter12.enummethods;
+
+public enum Apple {
+    JONATHAN,
+    GOLDEN_DEL,
+    RED_DEL,
+    WINESAP,
+    CORTLAND
+}
---
+++ src/chapters/chapter12/enummethods/EnumDemo3.java
@@ -0,0 +1,25 @@
+package chapters.chapter12.enummethods;
+
+public class EnumDemo3 {
+    public static void main(String[] args) {
+        Apple apple;
+        Apple apple1;
+        Apple apple2;
+        Apple[] values = Apple.values();
+        for (Apple value : values) {
+            System.out.println(value + " " + value.ordinal());
+        }
+        if (Apple.CORTLAND.compareTo(Apple.JONATHAN) < 0){
+            System.out.println(true);
+        }
+        if (Apple.RED_DEL.compareTo(Apple.JONATHAN) == 1){
+            System.out.println(true);
+        }
+        if (Apple.RED_DEL.equals(Apple.CORTLAND)){
+            System.out.println(true);
+        }
+        if (Apple.RED_DEL == Apple.RED_DEL){
+            System.out.println(true);
+        }
+    }
+}
