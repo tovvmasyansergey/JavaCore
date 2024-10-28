9922cc68b21f19f457f5725b6f826f6b9987117b
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-28 19:58:50 +0400

    increment by Integer

---
+++ src/chapters/chapter12/wraper/boxingunboxing/AutoBox3.java
@@ -0,0 +1,10 @@
+package chapters.chapter12.wraper.boxingunboxing;
+
+public class AutoBox3 {
+    public static void main(String[] args) {
+        Integer Iob, Iob1;
+        int i;
+        Iob = 100;
+        ++Iob;
+    }
+}
