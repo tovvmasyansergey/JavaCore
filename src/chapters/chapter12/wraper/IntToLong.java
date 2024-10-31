dca35e17de56603d529ac78c482091356c8ded32
tovvmasyansergey <161241614+tovvmasyansergey@users.noreply.github.com>
2024-10-31 20:01:47 +0400

    wraper class int to long

---
+++ src/chapters/chapter12/wraper/IntToLong.java
@@ -0,0 +1,8 @@
+package chapters.chapter12.wraper;
+
+public class IntToLong {
+    public static void main(String[] args) {
+        Integer iob = 10000;
+        long l = iob.longValue();
+    }
+}
